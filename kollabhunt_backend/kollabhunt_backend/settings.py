0c5b6dd0519e50c0998459e9c658f843758718c0
Shaffat-BS23 <Shaffat.Nur@brainstation-23.com>
2023-06-09 12:04:36 +0600

    fix DB settings

--- kollabhunt_backend/kollabhunt_backend/settings.py
+++ kollabhunt_backend/kollabhunt_backend/settings.py
@@ -127,11 +127,11 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": json.loads(os.environ.get('DATABASE_NAME')),
-            "USER": json.loads(os.environ.get('DATABASE_USER')),
-            "PASSWORD": json.loads(os.environ.get('DATABASE_PASSWORD')),
-            "HOST": json.loads(os.environ.get('DATABASE_HOST')),
-            "PORT": json.loads(os.environ.get('DATABASE_PORT'))
+            "NAME": os.environ.get('DATABASE_NAME'),
+            "USER": os.environ.get('DATABASE_USER'),
+            "PASSWORD": os.environ.get('DATABASE_PASSWORD'),
+            "HOST": os.environ.get('DATABASE_HOST'),
+            "PORT": os.environ.get('DATABASE_PORT')
         }
     }
 
